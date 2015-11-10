1ed66e4bc1e088586cf2504d10c4718740e3e9f9
Griffin Hammer <grifhammer@gmail.com>
2015-11-10 10:31:39 -0500

    Initial Commit
    Initial Commit

---
+++ controller.js
@@ -0,0 +1,30 @@
+angular.module('myApp', []).controller('myController', function($scope){
+
+	$scope.items = [
+		{
+			name: 'Freddy',
+			desc: 'Made a cool football game'
+		},
+		{
+			name: 'Chance',
+			desc: 'Bootstrap Guy'
+		},
+		{
+			name: 'Will',
+			desc: 'The wig guy'
+		},
+		{
+			name: 'Stephen',
+			desc: 'Awesome Theme!'
+		},
+		{
+			name: 'Griffin',
+			desc: 'DOTA2 addict'
+		},
+		{
+			name: 'Oliver',
+			desc: 'Eats 8 times a day'
+		}
+	]
+	
+});
